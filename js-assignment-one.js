29e41e7163c3b2b96bb4d45209c0940a996dc2f9
sedat-can <75433913+sedat-can@users.noreply.github.com>
2024-07-07 18:35:10 +0200

    Add files via upload

---
+++ js-assignment-one.js
@@ -0,0 +1,46 @@
+var age = 15;
+var name = "All";
+var isStudent = true;
+
+console.log(name + +" "+ age + " years olds. is a " + isStudent)
+
+//  task 2 */
+
+var a = 12
+var b = 8
+
+var result = null
+
+var result = a + b
+console.log(result)
+
+var result = a - b
+console.log(result)
+
+var result = a * b
+console.log(result)
+
+var result = a / b
+console.log(result)
+
+//  task 3 */
+
+var number = 1;
+var stringnum = "s12";
+console.log( number == stringnum)
+console.log( number === stringnum)
+console.log( number != stringnum)
+console.log( number !== stringnum)
+console.log( number > stringnum)
+console.log( number < stringnum)
+console.log( number >= stringnum)
+console.log( number >= stringnum)
+
+//  task 4 */
+
+var numberString = "10"
+var actualNumber = 5
+console.log(numberString + actualNumber)
+console.log(numberString - actualNumber)
+console.log(numberString * actualNumber)
+console.log(numberString / actualNumber)
